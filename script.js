d3b5dea5b687fc3dc6d2015304325850ae7e6e99
Makhmadali <101576757+Makhmadali@users.noreply.github.com>
2022-05-16 20:31:54 +0300

    new

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let movieName = document.getElementById('name')
 let movieRating = document.getElementById('rating')
 let selectCategories = document.getElementById('categories')
 let btn = document.querySelector('.btn')
+let movieList = document.querySelector('.movies')
 console.log(movies)
 // console.log(categories);
 
@@ -19,7 +20,40 @@ Categories.forEach(e => {
     selectCategories.appendChild(OPTION)
 })
 
-btn.addEventListener('click' , e =>{
-    console.log(movieName.value)
-    if (Categories)
+let renderMovies = (arr) =>{
+    arr.forEach(e =>{
+        let CARDWRAPPER = document.createElement('div')
+        CARDWRAPPER.classList.add('card')
+        CARDWRAPPER.classList.add('col-6')
+        
+        let IMAGE = document.createElement('img')
+        IMAGE.src = e.smallThumbnail
+        IMAGE.classList.add('card-image-top')
+
+        CARDWRAPPER.appendChild(IMAGE)
+
+        let CARDBODY = document.createElement('div')
+        CARDBODY.classList.add('card-body')
+
+        CARDWRAPPER.appendChild(CARDBODY)
+
+        let TITLE = document.createElement('h5')
+        TITLE.classList.add('card-title')
+        TITLE.textContent = e.title
+
+        CARDBODY.appendChild(TITLE)
+
+        movieList.appendChild(CARDWRAPPER)
+    })
+}
+
+btn.addEventListener('click' , () =>{
+    console.log(movieName.value);
+    const myReg = new RegExp(`${movieName.value}`, "gi")
+    // console.log(myReg);
+    let filterMovies = movies.filter((e) => myReg.test(e.title))
+
+    console.log(filterMovies);
+    
+    renderMovies(filterMovies)
 })
