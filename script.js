91e8ae4863a7144723632560b36b09c5eb998617
Makhmadali <101576757+Makhmadali@users.noreply.github.com>
2022-05-16 13:00:58 +0300

    ooo

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let movieName = document.getElementById('name')
 let movieRating = document.getElementById('rating')
 let selectCategories = document.getElementById('categories')
-
+let btn = document.querySelector('.btn')
 console.log(movies)
+// console.log(categories);
+
 
 let Categories = []
+
 movies.forEach(e =>{
     e.categories.forEach(el=> !Categories.includes(el) ? Categories.push(el) : Categories)
 })
+
 
 Categories.forEach(e => {
     let OPTION = document.createElement('option')
     OPTION.textContent = e
     selectCategories.appendChild(OPTION)
 })
-console.log(Categories);
+
+btn.addEventListener('click' , e =>{
+    console.log(movieName.value)
+    if (Categories)
+})
